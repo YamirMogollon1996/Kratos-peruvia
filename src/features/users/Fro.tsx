5d382fb236e7d6c643055bf90ae611d528f526bb
yamirmogollon <mogollonosorioyamir@gmail.com>
2025-12-18 07:49:12 -0500

    se agrego el div con la descripcion

--- src/features/users/Fro.tsx
+++ src/features/users/Fro.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const detalle  = [ {
-  id:  1 , numero : 10 , email :  'senior@gamial.com'
-}]
+const detalle = [
+  {
+    id: 1,
+    numero: 10,
+    email: "senior@gamial.com",
+  },
+];
 const Fro = () => {
-  return (  
+  return (
     <>
-<div>Fro</div>
+      <div>Fro</div>
 
       <div>
-          {
-            detalle.map((item , index) =>{
-                 return <div>
-                      <h1>{item.numero}</h1>
-                        <h1>{item.email}</h1>
-                          <h1>{item.id}</h1>
-                  </div>
-            })
-          }
-
-
+        {detalle.map((item, index) => {
+          return (
+            <div>
+              <h1>{item.numero}</h1>
+              <h1>{item.email}</h1>
+              <h1>{item.id}</h1>
+            </div>
+          );
+        })}
       </div>
     </>
+  );
+};
 
-
- 
-
-)
-}
-
-export default Fro
+export default Fro;
