010df50478b3edc69c248c1eafed63e813f6407b
yamirmogollon <mogollonosorioyamir@gmail.com>
2025-12-18 07:29:29 -0500

    se agrego el mensaje

---
+++ src/features/users/Fro.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const detalle  = [ {
+  id:  1 , numero : 10 , email :  'senior@gamial.com'
+}]
+const Fro = () => {
+  return (  
+    <>
+<div>Fro</div>
+
+      <div>
+          {
+            detalle.map((item , index) =>{
+                 return <div>
+                      <h1>{item.numero}</h1>
+                        <h1>{item.email}</h1>
+                          <h1>{item.id}</h1>
+                  </div>
+            })
+          }
+
+
+      </div>
+    </>
+
+
+ 
+
+)
+}
+
+export default Fro
